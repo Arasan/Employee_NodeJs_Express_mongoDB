6c6899279a968195900c2e937a6d029ebaa35484
Elavarasan Gandhi -T (elgandhi - INFOSYS LIMITED at Cisco) <elgandhi@cisco.com>
2022-08-09 19:54:44 +0530

    update & delete

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,5 @@
 const modelEmployee = require("../model/employee.model");
-
+/* get all employee details */
 exports.getallemployee = function(req, res) {
     modelEmployee.find().then(data => {
             res.send(data);
@@ -10,6 +10,8 @@ exports.getallemployee = function(req, res) {
             });
         });
 }
+
+/* create new  employee details */
 exports.createemployee = function(req, res) {
     const savedetails = new modelEmployee({
         name: req.body.name,
@@ -24,4 +26,43 @@ exports.createemployee = function(req, res) {
             message: error.message
         })
     }
+}
+
+/* get employee details by ID */
+exports.getbyID = function(req, res) {
+    modelEmployee.findById(req.params.id).then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while getting the employee."
+            });
+        });
+}
+
+/* update employee details by ID */
+exports.updatebyID = function(req, res) {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+    modelEmployee.findByIdAndUpdate(id, updatedData, options).then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while getting the employee."
+            });
+        });
+}
+/* delete employee details by ID */
+exports.deleteemployee = function(req, res) {
+    const id = req.params.id;
+   modelEmployee.findByIdAndDelete(id).then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while getting the employee."
+            });
+        });
 }
--- routes/router.js
+++ routes/router.js
@@ -4,5 +4,8 @@ const router = express.Router()
 
 router.get('/employee',employees.getallemployee);
 router.post('/employee',employees.createemployee);
+router.get('/employee/:id',employees.getbyID);
+router.put('/employee/:id',employees.updatebyID);
+router.delete('/employee/:id',employees.deleteemployee);
 
 module.exports = router;
